6dbd801a7b0dd5b6f8bde47314b14ecd261103c9
shaikahmed2611 <ahmedsk2611@gmail.com>
2024-09-02 21:47:42 +0530

    Delete TicketInterface.java

--- TicketInterface.java
+++
@@ -1,9 +0,0 @@
-package com.evergent.corejava.CaseStudyOfQueue;
-
-public interface TicketInterface {
-	public void addTicket(Ticket t1);
-	public void processNextTicket();
-	public void peekNextTicket();
-	public void displayQueue();
-	
-}
