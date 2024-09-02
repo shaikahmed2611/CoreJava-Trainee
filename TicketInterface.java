9c5afade1d416e771f01f4bd37930179f696e7d2
shaikahmed2611 <ahmedsk2611@gmail.com>
2024-09-02 21:43:54 +0530

    Add files via upload

---
+++ TicketInterface.java
@@ -0,0 +1,9 @@
+package com.evergent.corejava.CaseStudyOfQueue;
+
+public interface TicketInterface {
+	public void addTicket(Ticket t1);
+	public void processNextTicket();
+	public void peekNextTicket();
+	public void displayQueue();
+	
+}
